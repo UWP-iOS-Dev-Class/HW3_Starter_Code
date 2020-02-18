831e3fd3ee1b262bf6817b3eaef3bbbe7146db1f
Colin Hoffman <c.hoffman2009@yahoo.com>
2020-02-18 17:28:45 -0600

    Starter Code For the Assignment

---
+++ Homework3-SP20/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework3-SP20
+//
+//  Created by Colin Hoffman on 2/18/20.
+//  Copyright © 2020 iOS Class. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//Example of a function - takes in 3 string variables and returns a string
+//func checkWin(varName: String, varName: String, varName: String) -> String {}
